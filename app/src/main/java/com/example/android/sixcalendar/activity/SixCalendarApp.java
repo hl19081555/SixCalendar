23c0fab09895bf79a686702a07371d49f5401d62
lin.huang <lin.huang@tpv-tech.com>
2019-01-19 13:25:53 +0800

    增加异常捕获和申报， Bugly

--- app/src/main/java/com/example/android/sixcalendar/activity/SixCalendarApp.java
+++ app/src/main/java/com/example/android/sixcalendar/activity/SixCalendarApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.example.android.sixcalendar.libbaidu.BaiduAudioManager;
+import com.tencent.bugly.crashreport.CrashReport;
 
 public class SixCalendarApp extends Application {
     private static Context sInstance;
@@ -12,6 +13,14 @@ public class SixCalendarApp extends Application {
     public void onCreate() {
         super.onCreate();
         sInstance = this;
+
+        /* Bugly SDK初始化
+        * 参数1：上下文对象
+        * 参数2：APPID，平台注册时得到,注意替换成你的appId
+        * 参数3：是否开启调试模式，调试模式下会输出'CrashReport'tag的日志
+        */
+        CrashReport.initCrashReport(getApplicationContext(), "bc8f4b07ff", true);
+
         BaiduAudioManager.getInstance();
     }
 
